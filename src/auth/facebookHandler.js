1e4f4e788501dd5e365ee048757584f81abccab3
nhuhung19 <nhuhung19@gmail.com>
2020-06-03 18:15:56 +0700

    deploy again

--- src/auth/facebookHandler.js
+++ src/auth/facebookHandler.js
@@ -6,8 +6,8 @@ exports.loginFacebook = passport.authenticate("facebook", {scope: ['email']})
 
 exports.facebookAuth = function (req, res, next) {
     passport.authenticate("facebook", function (err, user) {
-        if(err) return res.redirect("https://localhost:3000/login")
+        if(err) return res.redirect("https://e-commerce-caketime.netlify.app/login")
     //    console.log("facebook=========", user)
-       return res.redirect(`https://localhost:3000/?token=${user.tokens[user.tokens.length-1]}`)
+       return res.redirect(`https://e-commerce-caketime.netlify.app/?token=${user.tokens[user.tokens.length-1]}`)
     })(req, res, next);
 }
--- src/auth/googleHandler.js
+++ src/auth/googleHandler.js
@@ -5,8 +5,8 @@ exports.loginGoogle = passport.authenticate("google")
 
 exports.googleAuth = function (req, res, next) {
     passport.authenticate("google", function (err, user) {
-       if(err) return res.redirect("https://localhost:3000/login")
+       if(err) return res.redirect("https://e-commerce-caketime.netlify.app/login")
     //    console.log(user)
-       return res.redirect(`https://localhost:3000/?token=${user.tokens[user.tokens.length-1]}`)
+       return res.redirect(`https://e-commerce-caketime.netlify.app/?token=${user.tokens[user.tokens.length-1]}`)
     })(req, res, next);
 }
